56be3287ce3179f3c384ebd8163e9f3e9bcd61e0
Altynay Zhaxylykova <66684636+zhaxylykova@users.noreply.github.com>
2021-02-24 02:59:50 +0600

    Add files via upload

---
+++ lab4/src/app/products.ts
@@ -0,0 +1,99 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Funsaver One Time Use Film Camera (2-pack)',
+    image: "https://images-na.ssl-images-amazon.com/images/I/61MdYtLK%2BUL._AC_SL1000_.jpg",
+    price: "$24.94",
+    description: 'Item Dimensions (LxWxH): 1.8 x 5.5 x 6.5 inches; item weight: 0.55 pounds',
+    rating: "★★★★★",
+    source: "https://www.amazon.com/Funsaver-Time-Film-Camera-2-pack/dp/B000HZDYAG"
+  },
+  {
+    id: 2,
+    name: 'New Kodak Weekend Underwater Disposable Camera Excellent Performance',
+    image: "https://images-na.ssl-images-amazon.com/images/I/51MTes3FV1L._AC_.jpg",
+    price: "$15.69",
+    description: 'This item has high quality with reasonable price. Exceptional customer service and unparalleled product expertise. Manufactured to the Highest Quality Available. 100% genuine brand names.',
+    rating: "★★★★☆",
+    source: "https://www.amazon.com/Kodak-Underwater-Disposable-Excellent-Performance/dp/B005M9O1DQ"
+  },
+  {
+    id: 3,
+    name: 'Kodak SUC Daylight 39 800iso Disposable Analog Camera',
+    image: "https://images-na.ssl-images-amazon.com/images/I/713aD6r5zfL._AC_SL1272_.jpg",
+    price: "$13.99",
+    description: 'Disposable camera without flash. 800 ISO film 39 Exposures Kodak. Range of 1 m to infinity. 32.8 mm, F10, 1 lens element.',
+    rating: "★★★★☆",
+    source: "https://www.amazon.com/Kodak-Daylight-39-800iso-Disposable-Analog/dp/B07D4L3C6B"
+  },
+  {
+    id: 4,
+    name: 'Fujifilm QuickSnap Flash 400 Disposable 35mm Camera (3 Pack)',
+    image: "https://images-na.ssl-images-amazon.com/images/I/71HD0Hur6IL._AC_SL1500_.jpg",
+    price: "$49.99",
+    description: 'One-Time-Use disposable camera in a compact body. Plus camera wrist strap and microfiber cloth. Great quality, sharp pictures with a 10-foot built-in-flash. Fun and easy to use. Perfect for all fun occasions! Great for indoor and outdoor pictures alike. Loaded with Superia X-TRA 400 film, 27 exposures',
+    rating: "★★★★★",
+    source: "https://www.amazon.com/Fujifilm-QuickSnap-Disposable-Quality-Microfiber/dp/B07N8GCYQB"
+  },
+  {
+    id: 5,
+    name: 'Fujifilm Instax Mini 11 Instant Camera',
+    image: "https://images-na.ssl-images-amazon.com/images/I/81e1OmzjWsL._AC_SL1500_.jpg",
+    price: "$109.95",
+    description: 'Includes FujiFilm Instax Mini 11 Camera Ice White, The Fujifilm Instax Mini 11 Camera Ice White features a Fujinon 60mm f/12.7 Lens, Optical Image Viewfinder, Auto Exposure and a Built-In Flash. Instantly produces image prints fun and simple. Fujifilm Instax Mini 11 Camera BUNDLE is Colorful, Fun, Compact and really Cool. Includes 40 Instax Film designed for shooting photos with a press of a button and instantly printing it out with the Fujifilm Mini series, enjoy to watch the images develop practically before your eyes. A unique Fujifilm fitted case with a removable strap for extra comfort was designed to both hold and protect your camera. Fashionable and practical. Be smart, be safe. Make the most of your instax mini 11 camera and enjoy the fun MiniMate accessories we’ve provided in this kit: Minimate Fujifilm instax mini 11. Keep your photos out for viewing, or store them neatly. We’re giving you both options, 64 Page Photo Album and 5 colored plastic frames.',
+    rating: "★★★★★",
+    source: "https://www.amazon.com/Fujifilm-Instant-Carrying-Accessories-Assorted/dp/B08NK8DH28/ref=sr_1_2_sspa?dchild=1&keywords=instax&qid=1614107416&s=photo&sr=1-2-spons&psc=1&smid=AND2O1F3ETQYZ&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExNDdFU0FSOUpYNTgyJmVuY3J5cHRlZElkPUEwNTM4NTQ5MVpPNDhNNFhNMzdRQyZlbmNyeXB0ZWRBZElkPUEwMjEwMDQyRDZMU01IODlOV0M5JndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="
+  },
+  {
+    id: 6,
+    name: 'KODAK Printomatic Digital Instant Print Camera',
+    image: "https://images-na.ssl-images-amazon.com/images/I/61w5iJxrgBL._AC_SL1500_.jpg",
+    price: "$49.99",
+    description: 'ALL-IN-ONE PHOTOGRAPY - The Point-and-Shoot Printomatic Camera offers a powerful 5MP sensor and wide angle f/2 lens. The Printomatic is the ideal all-in-one solution for capturing and sharing vibrant prints instantaneously wherever you are. ONE STOP PRINTING - The Kodak Printomatic camera instantly prints 2x3” photos. The camera uses Kodak Zink Photo Paper, so no ink cartridges, toners or film are needed. The photo prints are durable, water-resistant, tear resistant and smudge free.',
+    rating: "★★★★☆",
+    source: "https://www.amazon.com/Kodak-PRINTOMATIC-Digital-Instant-Sticky-Backed/dp/B07BB5V3HR/ref=sr_1_11_sspa?dchild=1&keywords=instax&qid=1614107416&s=photo&sr=1-11-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExNDdFU0FSOUpYNTgyJmVuY3J5cHRlZElkPUEwNTM4NTQ5MVpPNDhNNFhNMzdRQyZlbmNyeXB0ZWRBZElkPUEwMDc3MTQ1SkMzWEE4Rk9YU0FXJndpZGdldE5hbWU9c3BfbXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="
+  },
+  {
+    id: 7,
+    name: 'Fujifilm Instax Mini 11 Instant Camera',
+    image: "https://images-na.ssl-images-amazon.com/images/I/71qLk2El4NL._AC_SL1500_.jpg",
+    price: "$69.99",
+    description: 'Slim body size with transparent ring lens. Automatic Exposure - The Mini 11 automatically selects the optimal shutter speed in any environment. Mini 11 captures bright backgrounds and bright subjects, even in dark scenes. 1-touch Selfie Mode – The Mini 11 has a built-in macro mode and selfie mirror for the perfect selfie. You don’t need a close-up lens attachment. Just pull out the lens barrel until the “Selfie Mode” mark appears. Customize your camera - 2 shutter button accessories are included - Jewel button and a Glow button. 2 - AA Batteries',
+    rating: "★★★★★",
+    source: "https://www.amazon.com/Fujifilm-Instax-Mini-Instant-Camera/dp/B0852844YB/ref=sr_1_13?dchild=1&keywords=instax&qid=1614107416&s=photo&sr=1-13"
+  },
+  {
+    id: 8,
+    name: 'Fujifilm Instax Mini Instant Film Twin Pack ',
+    image: "https://images-na.ssl-images-amazon.com/images/I/813oF-FY01L._AC_SL1500_.jpg",
+    price: "$13.38",
+    description: 'Designed for use with Instax Mini line of cameras; Works well in low light. Develops instantly: Press the shutter, capture the moment, and watch the photo slide out and the image develop in front of your eyes; Hi speed, ISO 800 with superb grain quality, Instax Mini Film ensure vibrant color and natural skin tones.',
+    rating: "★★★★★",
+    source: "https://www.amazon.com/Fujifilm-INSTAX-Instant-Twin-Pack/dp/B00EB4ADQW/ref=pd_rhf_se_s_gcx-rhf_3?pd_rd_w=ADCoD&pf_rd_p=2ae4ccb1-7034-4114-8654-5ba995870d70&pf_rd_r=4SSK0CYXH8AQ6VFA2Q7B&pd_rd_r=3b6b01a9-e774-4138-8294-2d8ff032396f&pd_rd_wg=HUuoF&pd_rd_i=B00EB4ADQW&psc=1"
+  },
+  {
+    id: 9,
+    name: 'Video Camera Camcorder',
+    image: "https://images-na.ssl-images-amazon.com/images/I/71C6TFutbeL._AC_SL1500_.jpg",
+    price: "$63.99",
+    description: 'The video camera is useful to take the beautiful photos and share it in your Youtube. It can be used as webcam when the camera connect the computer. Please operate the camera button to choose the “PC CAM” mode.When the “AMCAP ” window is opened,from which shooting object through the camera will be showed on this window. You can have a video call with your families or friends. Please download the software “AMCAP ” before use the webcam function.',
+    rating: "★★★☆☆",
+    source: "https://www.amazon.com/dp/B08K7HFMX4/ref=sspa_dk_detail_0?psc=1&pd_rd_i=B08K7HFMX4&pd_rd_w=HlWby&pf_rd_p=cbc856ed-1371-4f23-b89d-d3fb30edf66d&pd_rd_wg=xjQsL&pf_rd_r=J93SSBMKDM9RG94DTXA8&pd_rd_r=3b2a04f6-1426-4a27-8fe9-8a34dbda85c9&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEySE4xSlJUODA3TE5HJmVuY3J5cHRlZElkPUEwNjQxMTk3R0UyTTA0N1cySkZYJmVuY3J5cHRlZEFkSWQ9QTAyOTM1NjMzQTkxNzg1MTBESlZWJndpZGdldE5hbWU9c3BfZGV0YWlsX3RoZW1hdGljJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="
+  },
+  {
+    id: 10,
+    name: 'Megagear MG1281 Fujifilm X100F Ever Ready Genuine Leather Camera',
+    image: "https://images-na.ssl-images-amazon.com/images/I/71g0PjnsboL._AC_SL1500_.jpg",
+    price: "$21.31",
+    description: 'CONVENIENT - Convenience features allow rapid use of your camera when needed. Perfect for functional travel and outdoor use. STYLISH DESIGN - Whether you are a beginner or a seasoned, professional photographer, you will want these lightweight leather cases with exquisite, ageless leather style at your side.',
+    rating: "★★★★☆",
+    source: "https://www.amazon.com/Megagear-MG1281-Fujifilm-Genuine-Leather/dp/B074HH8P55/ref=pd_sbs_12?pd_rd_w=qU15g&pf_rd_p=3ec6a47e-bf65-49f8-80f7-0d7c7c7ce2ca&pf_rd_r=J93SSBMKDM9RG94DTXA8&pd_rd_r=3b2a04f6-1426-4a27-8fe9-8a34dbda85c9&pd_rd_wg=xjQsL&pd_rd_i=B074HH8P55&psc=1"
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
